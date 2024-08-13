8d1a244cdb21adfc3d7583dccbc0dff8fc30e8ff
Kanishkaaa08 <kanishkamathur08@gmail.com>
2024-08-13 15:50:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add interactivity or form validation here
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Message sent successfully!');
+});
